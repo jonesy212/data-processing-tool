eedb0dfcf0598cfb241a813384e377b68b537659
Marcus Jones <indelibleink84@gmail.com>
2025-03-17 15:32:42 -0500

    Remove deleted files

--- setup_root.sh
+++ setup_root.sh
@@ -8,9 +8,9 @@ select root_directory in */; do
             echo "Setting root to frontend"
             # Your logic to set the root here, e.g., write to a configuration file
             break
-            ;;
+            ;
         * )
             echo "Invalid selection, please choose a valid option."
-            ;;
+            ;
     esac
 done
